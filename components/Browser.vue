c6c6d576fe4c66c5b4e5fab3eb2bf4d90e2010af
AWeirdDev <aweirdscratcher@gmail.com>
2026-07-03 19:15:07 +0800

    you CANT alter thoghts??? bruh

--- components/Browser.vue
+++ components/Browser.vue
@@ -11,7 +11,9 @@ const url = ref(props.url);
 </script>
 
 <template>
-    <div class="border-0.5 border-solid border-gray-200 rounded-lg bg-white">
+    <div
+        class="border-0.5 border-solid border-gray-200 rounded-lg bg-white dark:bg-black/80 dark:border-white/20 dark:text-black/80"
+    >
         <div class="p-2">
             <div class="flex flex-row items-center gap-1">
                 <div class="w-2 h-2 bg-red-500 rounded-full"></div>
@@ -21,16 +23,18 @@ const url = ref(props.url);
         </div>
 
         <div
-            class="text-xs px-4 h-8 border-y-0.5 border-solid border-y-gray-200 flex flex-items items-center"
+            class="text-xs px-4 h-8 border-y-0.5 border-solid border-y-gray-200 dark:border-y-white/20 flex flex-items items-center"
         >
-            <div class="flex flex-items items-center gap-2 text-[12px]">
+            <div
+                class="flex flex-items items-center gap-2 text-[12px] dark:text-white/60"
+            >
                 <lucide-arrow-left />
                 <lucide-arrow-right />
                 <lucide-rotate-ccw />
             </div>
 
             <div
-                class="ml-4 w-full bg-gray-100 px-2 py-1 rounded-md flex flex-items items-center gap-2"
+                class="ml-4 w-full bg-gray-100 px-2 py-1 rounded-md flex flex-items items-center gap-2 dark:bg-transparent dark:text-white/60 dark:border dark:border-solid dark:border-white/10"
             >
                 <lucide-lock-keyhole class="text-[10px]" />
                 {{ url }}
